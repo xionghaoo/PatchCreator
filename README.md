c1390e46e9cff7faf4550d5105fe000a20628e87
xionghao <xionghao930316@gmail.com>
2019-05-09 16:44:30 +0800

    fix bug

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 补丁生成器
+> 用来生成ReactNative热更新的补丁文件
+
+用到的工具: [diff-match-patch](https://github.com/google/diff-match-patch)
+
+补丁生成脚本: patch_diff.py
--- patch_diff.py
+++ patch_diff.py
@@ -2,18 +2,27 @@ import os
 import diff_match_patch as dmp_module
 import codecs
 
+# 原版本文件
+OLD_FILE = 'index.android.bundle'
+
+# 新版本文件
+NEW_FILE = 'index.android.bundle_2'
+
+# 补丁文件
+PATCH_FILE = '_patch'
+
 
 def create_pat_file(f1, f2):
-    print('--------start--------')
+    print('--------patch start--------')
     f1_text = read_file(f1)
     f2_text = read_file(f2)
 
     dmp = dmp_module.diff_match_patch()
     diff = dmp.diff_main(f1_text, f2_text)
     patches = dmp.patch_toText(dmp.patch_make(diff))
-    write_file('patch.pat', patches)
-    print('patch.bat created')
-    print('--------end--------')
+    write_file(PATCH_FILE, patches)
+    print(PATCH_FILE + ' file created')
+    print('--------patch end--------')
 
 
 def read_file(f):
@@ -31,6 +40,7 @@ def write_file(f_name, content):
 
 
 def verify_patch(f_patch, f1):
+    print('--------verify start--------')
     patches_text = read_file(f_patch)
     f1_text = read_file(f1)
     dmp = dmp_module.diff_match_patch()
@@ -41,10 +51,11 @@ def verify_patch(f_patch, f1):
         print('patch verify success')
     else:
         print('patch verify failure')
+    print('--------verify end--------')
 
 
-create_pat_file('f1', 'f2')
+create_pat_file(OLD_FILE, NEW_FILE)
 
-verify_patch('patch.pat', 'f1')
+verify_patch(PATCH_FILE, OLD_FILE)
 
 
